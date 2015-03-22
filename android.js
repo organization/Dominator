65ac2cc1b8a75574f6fb6add198093707e910ada
ChalkPE <amato0617@gmail.com>
2015-03-22 22:00:58 +0900

    Added some libraries

---
+++ android.js
@@ -0,0 +1,67 @@
+/**
+ * Created by ChalkPE on 2015-03-22.
+ */
+
+var android = {};
+android.graphics = {};
+android.graphics.Bitmap = function(){};
+android.graphics.Bitmap.createBitmap = function(){};
+android.graphics.Canvas = function(){};
+android.graphics.Canvas.drawRect = function(){};
+android.graphics.Canvas.drawText = function(){};
+android.graphics.Color = function(){};
+android.graphics.Color.parseColor = function(){};
+android.graphics.Color.rgb = function(){};
+android.graphics.Color.TRANSPARENT = NaN;
+android.graphics.Paint = function(){};
+android.graphics.Paint.Style = function(){};
+android.graphics.Paint.Style.STROKE = NaN;
+android.graphics.Paint.setStrokeWidth = function(){};
+android.graphics.Paint.setStyle = function(){};
+android.graphics.Paint.setStyle = function(){};
+android.graphics.Paint.setStyle = function(){};
+android.os = {};
+android.os.Environment = function(){};
+android.os.Environment.getExternalStorageDirectory = function(){};
+android.view = {};
+android.view.View = function(){};
+android.view.View.OnClickListener = function(){};
+android.view.View.OnTouchListener = function(){};
+android.view.ViewGroup = function(){};
+android.view.ViewGroup.LayoutParams = function(){};
+android.view.ViewGroup.LayoutParams.WRAP_CONTENT = NaN;
+android.widget = {};
+android.widget.PopupWindow = function(){};
+android.widget.PopupWindow.dismiss = function(){};
+android.widget.PopupWindow.showAtLocation = function(){};
+android.widget.PopupWindow.setFocusable = function(){};
+android.widget.PopupWindow.setTouchable = function(){};
+android.widget.PopupWindow.setWindowLayoutMode = function(){};
+android.widget.PopupWindow.setWidth = function(){};
+android.widget.Pro = function(){};
+android.widget.ImageView = function(){};
+android.widget.ImageView.setImageBitmap = function(){};
+android.widget.ImageView.setOnTouchListener = function(){};
+android.widget.RelativeLayout = function(){};
+android.widget.RelativeLayout.addView = function(){};
+android.widget.TextView = function(){};
+android.widget.TextView.setText = function(){};
+android.widget.TextView.setBackgroundColor = function(){};
+android.widget.TextView.setTextSize = function(){};
+android.widget.TextView.setTextColor = function(){};
+android.widget.TextView.setX = function(){};
+android.widget.TextView.setY = function(){};
+android.widget.Button = function(){};
+android.widget.Button.setText = function(){};
+android.widget.Button.setLayoutParams = function(){};
+android.widget.Button.setOnClickListener = function(){};
+android.widget.Button.setText = function(){};
+android.widget.Button.setText = function(){};
+
+
+var com = {};
+com.mojang = {};
+com.mojang.minecraftpe = {};
+com.mojang.minecraftpe.MainActivity = function(){};
+com.mojang.minecraftpe.MainActivity.currentMainActivity = {};
+com.mojang.minecraftpe.MainActivity.currentMainActivity.get = function(){};
---
+++ java.js
@@ -0,0 +1,17 @@
+/**
+ * Created by ChalkPE on 2015-03-22.
+ */
+
+var java = {};
+java.io = {};
+java.io.File = function(){};
+java.lang = {};
+java.lang.Runnable = function(){};
+java.lang.System = function(){};
+java.lang.System.currentTimeMillis = function(){};
+java.lang.Thread = function(){};
+java.lang.Thread.start = function(){};
+java.lang.Thread.sleep = function(){};
+java.util = {};
+java.util.Random = function(){};
+java.util.Random.nextInt = function(){};
